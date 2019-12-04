d9113d82f1279185da02b7e3de4287d8d3c427b9
Nik <nikvaes@gmail.com>
2019-12-04 11:05:15 +0100

    spacing between end-of-line separator...

--- experiments/deepfashion2/train_deepfashion2.sh
+++ experiments/deepfashion2/train_deepfashion2.sh
@@ -1,7 +1,7 @@
 python3 ../../robotfashion/robotfashion/models/faster_rcnn/trainer.py \
   --gpus 0 \
   --nodes 1 \
-  --num-data-loaders 4\
+  --num-data-loaders 4 \
   --max_nb_epochs 100 \
   --batch-size 10 \
   --data-folder-path .. \
