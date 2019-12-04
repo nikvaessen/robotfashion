372f15ee1c633f15c33f5be2bb4c216cb09edcc3
Nik <nikvaes@gmail.com>
2019-12-04 11:12:35 +0100

    batch size should be smaller

--- experiments/deepfashion2/train_deepfashion2.sh
+++ experiments/deepfashion2/train_deepfashion2.sh
@@ -3,7 +3,7 @@ python3 ../../robotfashion/robotfashion/models/faster_rcnn/trainer.py \
   --nodes 1 \
   --num-data-loaders 4 \
   --max_nb_epochs 100 \
-  --batch-size 10 \
+  --batch-size 5 \
   --data-folder-path .. \
   --save-weights-every-n 1 \
   --data-folder-path .. \
