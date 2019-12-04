04dc604abcd9afcf5d6ece8e1a66c085cc15410a
Nik <nikvaes@gmail.com>
2019-12-04 23:31:56 +0100

    train on robotfashion with and without freezing backbone

--- experiments/robotfashion/train_robotfashion.sh
+++ experiments/robotfashion/train_robotfashion.sh
@@ -6,8 +6,18 @@ python3 ../../robotfashion/robotfashion/models/faster_rcnn/trainer.py \
   --batch-size 8 \
   --data-folder-path .. \
   --save-weights-every-n 1 \
-  --subset-ratio 0.1 \
   --freeze-backbone True \
+  --dataset "robotfashion"
+
+python3 ../../robotfashion/robotfashion/models/faster_rcnn/trainer.py \
+  --gpus 0 \
+  --nodes 1 \
+  --num-data-loaders 4 \
+  --max_nb_epochs 100 \
+  --batch-size 8 \
+  --data-folder-path .. \
+  --save-weights-every-n 1 \
+  --freeze-backbone False \
   --dataset "robotfashion"
 
 sudo shutdown now
